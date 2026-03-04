a38779a239101505145cc56a4bbbf9283ac309cc
evolflow <flowvolodymyr@gmail.com>
2026-03-04 15:53:19 +0100

    Coding Session 2 - JavaScript DOM interactions

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const btn = document.getElementById("contactBtn");
+let button = document.getElementById("btn");
+let box = document.getElementById("box");
 
-btn.addEventListener("click", function() {
-    alert("You can contact me soon 🚀");
+button.addEventListener("click", function() {
+    box.classList.toggle("show");
+
+    if (box.classList.contains("show")) {
+        button.textContent = "Close box";
+    } else {
+        button.textContent = "Open box";
+    }
+});
+
+    box.addEventListener("mouseover", function() {
+        box.style.background = "orange";
+    });
+
+    box.addEventListener("mouseout", function() {
+        box.style.background = "lightblue";
+    
+});
+
+let colorButton = document.getElementById("colorBtn");
+
+colorButton.addEventListener("click", function() {
+    box.style.background = "green";
 });
